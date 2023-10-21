62785031789c1ddacb33554074dfa14f1f5cdbfc
Amier Ali <amier_1998@hotmail.com>
2023-10-21 22:49:09 +0100

    feat: Implement rest parameter to add numbers using for loop

--- modern_javascript_exercises/rest_parameters/rest_parameters.js
+++ modern_javascript_exercises/rest_parameters/rest_parameters.js
@@ -1,9 +1,13 @@
 // Instructions can be found in rest_parameters.md
 
 // Add rest parameters here!
-export function add(a, b) {
+export function add(...args) {
   // Add a loop here
-  return a + b;
+  let sum = 0;
+  for (const numbers of args) {
+    sum += numbers;
+  }
+  return sum;
 }
 
 add(1, 2, 3, 4, 5);
